43a67eaeb8eaf97e9defd97221c8782d94e51de0
hamza-shezad <hamza.shezad@xstak.com>
2023-04-06 12:33:31 +0500

    XPAY-101 feat(world): add tests for anotherworld

--- __tests__/World.test.ts
+++ __tests__/World.test.ts
@@ -1,7 +1,11 @@
-import { World } from "../src/World";
+import { AnotherWorld, World } from "../src/World";
 
 describe("World", () => {
     test("World returns 'World'", () => {
         expect(World()).toBe("World");
+    });
+
+    test("AnotherWorld returns 'AnotherWorld'", () => {
+        expect(AnotherWorld()).toBe("AnotherWorld");
     });
 });
